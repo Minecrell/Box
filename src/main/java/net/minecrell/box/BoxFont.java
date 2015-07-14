8b0ab2cf9975473b110d1262a84c505e18b14954
Minecrell <dev@minecrell.net>
2015-07-14 22:32:46 +0200

    Fix copyright year

--- src/main/java/net/minecrell/box/BoxFont.java
+++ src/main/java/net/minecrell/box/BoxFont.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Minecrell <https://github.com/Minecrell>
+ * Copyright (c) 2014, Minecrell <https://github.com/Minecrell>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
